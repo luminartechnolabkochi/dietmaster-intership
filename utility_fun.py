4b6162f92056d0f2bd16903e51c179a97748d240
luminartechnolabkochi <84428864+luminartechnolabkochi@users.noreply.github.com>
2026-05-04 09:02:40 +0530

    Add files via upload

---
+++ utility_fun.py
@@ -0,0 +1,21 @@
+
+
+def daily_calorie_consumption(height,weight,age,gender="male",activity_level=1.2):
+
+    bmr = 0
+
+
+    if gender=="male":
+
+        bmr = 10*weight+6.25*height-5*age+5
+
+    else:
+        bmr = 10*weight+6.25*height-5*age-161
+
+    
+    calorie=bmr*activity_level
+
+    return round(calorie)
+
+
+# print(daily_calorie_consumption(165,75,32,"male",1.2))
